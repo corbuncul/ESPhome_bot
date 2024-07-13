65b00c03062778fb265b74d4ccd03f222433e434
Иван Агафонов <agafivan@yandex.ru>
2024-07-13 23:43:13 +0300

    Изменено на функции с декораторами. Данные отправляет только по запросу.

--- esphomebot.py
+++ esphomebot.py
@@ -5,20 +5,15 @@ from logging import StreamHandler
 import os
 import requests
 import sys
-import time
 
 from dotenv import load_dotenv
-from telebot import TeleBot
-from telebot.apihelper import ApiException
+from telebot import TeleBot, types
 
 from exceptions import (
     BaseEnvError,
     GetAnswerFromAPIError,
-    SendMessageError
 )
 
-
-RETRY_PERIOD = 600
 
 logger = logging.getLogger('ESPHome_Bot')
 formatter = logging.Formatter(
@@ -29,145 +24,144 @@ handler = StreamHandler(stream=sys.stdout)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
+load_dotenv()
 
-class ESPHomeBot:
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+BASE_NAME = 'http://esptemppres.local/sensor/'
+ENDPOINTS = [
+    'bmp280_pres',
+    'bmp280_temp',
+    'dallas_temp_1',
+    'dallas_temp_2',
+    'dht_temp',
+    'dht_hum',
+]
+HELP_MESSAGE = (
+    'Привет! Я бот, который выступает посредником между ESPHome и тобой.\n'
+    'В ответ на команды, я запрашиваю данные с ESPHome и оправляю ответ тебе.'
+)
+
+
+def check_tokens():
     """
-    Класс бота для общения с ESPHome.
-    Пока ничего не умеет)).
+    Проверка токенов.
+
+    Осуществляет проверку токенов Telegram-боту,
+    а также наличие ID чата с пользователем.
+    В случае их отсутсвия вызываются исключение BaseEnvError.
+
+    Args:
+        None
+
+    Returns:
+        None
     """
+    tokens = {
+        'TELEGRAM_TOKEN': TELEGRAM_TOKEN,
+        'TELEGRAM_CHAT_ID': TELEGRAM_CHAT_ID
+    }
+    error_msg = ''
+    for token_name, token_value in tokens.items():
+        if token_value is None:
+            error_msg += f'Отсутсвует переменная окружения {token_name}.\n'
+    if error_msg != '':
+        logger.critical(error_msg, exc_info=True)
+        raise BaseEnvError(error_msg)
+    logger.debug('Переменные окружения в порядке.')
 
-    def __init__(self) -> None:
-        """
-        Инициализируем переменные окружения и эндпоинты.
-        """
-        load_dotenv()
 
-        self.TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-        self.TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-        self.BASE_NAME = 'http://esptemppres.local/sensor/'
-        self.ENDPOINTS = [
-            'bmp280_pres',
-            'bmp280_temp',
-            'dallas_temp_1',
-            'dallas_temp_2',
-            'dht_temp',
-            'dht_hum',
-        ]
-        self.check_tokens()
-        self.bot = TeleBot(token=self.TELEGRAM_TOKEN)
+def check_boss_id(message):
+    """Проверка на владельца бота."""
+    return message.chat.id == int(TELEGRAM_CHAT_ID)
 
-    def check_tokens(self):
-        """
-        Проверка токенов.
 
-        Осуществляет проверку токенов к API Практикума и Telegram-боту,
-        а также наличие ID чата с пользователем.
-        В случае их отсутсвия вызываются исключение BaseEnvError.
+def get_api_answer():
+    """
+    Получение ответа от API ESPHome.
 
-        Args:
-            None
+    Осуществляет запрос к API ESPHome.
+    В случае ошибок получения ответа вызывает исключение
+    GetAnswerFromAPIError.
 
-        Returns:
-            None
-        """
-        tokens = {
-            'TELEGRAM_TOKEN': self.TELEGRAM_TOKEN,
-            'TELEGRAM_CHAT_ID': self.TELEGRAM_CHAT_ID
-        }
-        error_msg = ''
-        for token_name, token_value in tokens.items():
-            if token_value is None:
-                error_msg += f'Отсутсвует переменная окружения {token_name}.\n'
-        if error_msg != '':
-            logger.critical(error_msg, exc_info=True)
-            raise BaseEnvError(error_msg)
-        logger.debug('Переменные окружения в порядке.')
-
-    def send_message(self, message):
-        """
-        Отправка сообщения.
-
-        Отправляет сообщение через бота.
-        Отправляет в лог сообщение об отправке.
-        В случае ошибки отправки сообщения выбрасывает исключение
-        SendMessageError.
-
-        Args:
-            bot (TeleBot): экземпляр бота.
-            message (str): строка сообщения.
-
-        Returns:
-            None
-        """
+    Returns:
+        (dict): Объект json, приведенный к типам Python
+    """
+    results = []
+    bn = BASE_NAME
+    for endpoint in ENDPOINTS:
+        logger.debug(f'Соединение с {bn+endpoint}')
         try:
-            self.bot.send_message(chat_id=self.TELEGRAM_CHAT_ID, text=message)
-        except ApiException as error:
-            error_msg = f'Ошибка отправки сообщения: {error}'
-            raise SendMessageError(error_msg)
-        else:
-            logger.debug(f'Сообщение отправлено: {message}')
-        return True
+            response = requests.get(bn + endpoint)
+        except requests.RequestException as error:
+            error_msg = f'Ошибка соединения с API: {error}'
+            raise GetAnswerFromAPIError(error_msg)
 
-    def get_api_answer(self):
-        """
-        Получение ответа от API ESPHome.
+        status_code = response.status_code
+        if status_code != HTTPStatus.OK:
+            error_msg = f'Ошибка ответа HTTP status: {status_code}'
+            raise GetAnswerFromAPIError(error_msg)
 
-        Осуществляет запрос к API ESPHome.
-        В случае ошибок получения ответа вызывает исключение
-        GetAnswerFromAPIError.
+        logger.debug(f'Получен ответ от {bn+endpoint}')
 
-        Returns:
-            (dict): Объект json, приведенный к типам Python
-        """
-        self.results = []
-        bn = self.BASE_NAME
-        for endpoint in self.ENDPOINTS:
-            logger.debug(f'Соединение с {bn+endpoint}')
-            try:
-                response = requests.get(bn + endpoint)
-            except requests.RequestException as error:
-                error_msg = f'Ошибка соединения с API: {error}'
-                raise GetAnswerFromAPIError(error_msg)
-
-            status_code = response.status_code
-            if status_code != HTTPStatus.OK:
-                error_msg = f'Ошибка ответа HTTP status: {status_code}'
-                raise GetAnswerFromAPIError(error_msg)
-
-            logger.debug(f'Получен ответ от {bn+endpoint}')
-
-            try:
-                self.results.append(response.json())
-            except json.JSONDecodeError as error:
-                error_msg = f'Не валидный JSON: {error}'
-                raise GetAnswerFromAPIError(error_msg)
-
-    def send_answer(self):
-        """
-        Отсылает ответ от API ESPHome.
-        Все ответы от эндпоинтов отсылаются в телеграм.
-        """
-        message = ''
-        for result in self.results:
-            id = result.get('id')
-            state = result.get('state')
-            message += f'{id} = {state}\n'
-        self.send_message(message)
-
-
-def main():
-    """Основная логика работы бота."""
-    EHbot = ESPHomeBot()
-    while True:
         try:
-            EHbot.get_api_answer()
-            EHbot.send_answer()
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            logger.error(message, exc_info=True)
-            EHbot.send_message(message)
-        time.sleep(RETRY_PERIOD)
+            results.append(response.json())
+        except json.JSONDecodeError as error:
+            error_msg = f'Не валидный JSON: {error}'
+            raise GetAnswerFromAPIError(error_msg)
+    return results
 
 
-if __name__ == '__main__':
-    main()
+def make_tg_answer(results):
+    """Подготавливает ответ от API ESPHome."""
+    message = ''
+    for result in results:
+        id = result.get('id')[7:]
+        state = result.get('state')
+        message += f'{id} = {state}\n'
+    return message
+
+
+check_tokens()
+bot = TeleBot(token=TELEGRAM_TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def wake_up(message):
+    """
+    Функция приветствия.
+    В ответ на команду /start
+    отправляет сообщение приветственное сообщение.
+    """
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_start = types.KeyboardButton('/start')
+    keyboard.add(button_start)
+    button_sensors = types.KeyboardButton('/sensors')
+    keyboard.add(button_sensors)
+    if message.chat.id == int(TELEGRAM_CHAT_ID):
+        button_settings = types.KeyboardButton('/settings')
+        keyboard.add(button_settings)
+
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=HELP_MESSAGE,
+        reply_markup=keyboard,
+    )
+
+
+@bot.message_handler(commands=['sensors'])
+def sensors_data(message):
+    """Отправка данных с сенсоров в телеграм."""
+    bot.send_message(
+        chat_id=message.chat.id,
+        text=make_tg_answer(get_api_answer())
+    )
+
+
+@bot.message_handler(func=check_boss_id, commands=['settings'])
+def set_settings(message):
+    """Заготовка на будущее."""
+    bot.send_message(chat_id=message.chat.id, text='Тут пока ничего нет.')
+
+
+bot.polling()
